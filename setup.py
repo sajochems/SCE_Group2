36109891fe848f699119fd943c303dc901f361eb
Ting-Chia Chiang <t.chiang@vu.nl>
2024-10-09 23:45:07 +0200

    Update the setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,6 @@ local_requirements = [
     'scp',
 ]
 
-# Dependencies specific to each component or server
-extras_require = {
-    'dialogflow': [
-        'google-cloud-dialogflow',
-    ],
-    'face_detection_dnn': [
-        'matplotlib',
-        'pandas',
-        'pyyaml',
-        'torch',
-        'torchvision',
-        'tqdm',
-    ],
-    'local': local_requirements,
-}
-
 # Common requirements for both local machine and robot
 common_requirements = [
     'numpy',
@@ -33,12 +17,49 @@ common_requirements = [
     'six',
 ]
 
+# Dependencies specific to each component or server
+extras_require = {
+    'dialogflow': [
+        'google-cloud-dialogflow',
+    ],
+    'face-detection-dnn': [
+        'matplotlib',
+        'pandas',
+        'pyyaml',
+        'torch',
+        'torchvision',
+        'tqdm',
+    ],
+    'face-recognition-dnn': [
+        'scikit-learn',
+        'torch',
+        'torchvision',
+    ],
+    'local': local_requirements,
+}
+
 setup(
     name='social-interaction-cloud',
-    version='2.0.3',
+    version='2.0.0',
     author='Koen Hindriks',
     author_email='k.v.hindriks@vu.nl',
     packages=find_packages(),
+    package_data={
+        'sic_framework.services.face_detection': [
+            'haarcascade_frontalface_default.xml',
+        ],
+        'sic_framework.services.face_recognition_dnn': [
+            'haarcascade_frontalface_default.xml',
+            'resnet50_ft_weight.pt',
+        ],
+    },
     install_requires=common_requirements,
     extras_require=extras_require,
+    entry_points={
+        'console_scripts': [
+            'run-dialogflow=sic_framework.services.dialogflow:main',
+            'run-face-detection=sic_framework.services.face_detection:main',
+            'run-face-recognition=sic_framework.services.face_recognition_dnn:main',
+        ],
+    },
 )
