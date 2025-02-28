786ca29ed54c46c3545625bc196aaa1fa751cf2e
Ting-Chia Chiang <t.chiang@vu.nl>
2025-02-28 23:21:19 +0100

    Get rid of sox for playing audio, use pyaudio instead

--- sic_framework/devices/common_mini/mini_speaker.py
+++ sic_framework/devices/common_mini/mini_speaker.py
@@ -1,15 +1,11 @@
-import asyncio
-import os
-import wave
+import pyaudio
 
 from sic_framework import SICComponentManager
 from sic_framework.core.component_python2 import SICComponent
 from sic_framework.core.connector import SICConnector
-from sic_framework.core.message_python2 import AudioMessage, SICConfMessage, SICMessage
-
-import mini.pkg_tool as Tool
-
-from sic_framework.devices.common_mini.mini_connector import MiniConnector
+from sic_framework.core.message_python2 import (
+    AudioMessage, SICConfMessage, SICMessage
+)
 
 
 class MiniSpeakersConf(SICConfMessage):
@@ -21,9 +17,17 @@ class MiniSpeakersConf(SICConfMessage):
 class MiniSpeakerComponent(SICComponent):
     def __init__(self, *args, **kwargs):
         super(MiniSpeakerComponent, self).__init__(*args, **kwargs)
-        self.alphamini = MiniConnector()
-        self.alphamini.connect()
-        self.i = 0
+
+
+        self.device = pyaudio.PyAudio()
+        # open an audio stream
+        self.stream = self.device.open(
+            format=pyaudio.paInt16,
+            channels=self.params.channels,
+            rate=self.params.sample_rate,
+            input=False,
+            output=True,
+        )
 
     @staticmethod
     def get_conf():
@@ -31,50 +35,25 @@ class MiniSpeakerComponent(SICComponent):
 
     @staticmethod
     def get_inputs():
-        return []
+        return [AudioMessage]
 
     @staticmethod
     def get_output():
         return SICMessage
 
     def on_message(self, message):
-        self.play_sound(message)
+        self.stream.write(message.waveform)
 
     def on_request(self, request):
-        self.play_sound(request)
+        self.stream.write(request.waveform)
         return SICMessage()
 
-    def play_sound(self, message):
-        bytestream = message.waveform
-        frame_rate = message.sample_rate
+    def stop(self, *args):
+        super(MiniSpeakerComponent, self).stop(*args)
+        self.logger.info("Stopped speakers")
 
-        # # Create the directory tmp directory in the home folder if it doesn't exist
-        tmp_path = "/data/data/com.termux/files/home/tmp/"
-        os.makedirs(os.path.dirname(tmp_path), exist_ok=True)
-        # # Define the file path for the temp audio file
-        tmp_file = tmp_path + f"tmp{self.i}.wav"
-
-        # Set the parameters for the WAV file
-        channels = 1  # 1 for mono audio
-        sample_width = 2  # 2 bytes for 16-bit audio
-        num_frames = len(bytestream) // (channels * sample_width)
-
-        with wave.open(tmp_file, "wb") as wav_file:
-            wav_file.setparams(
-                (channels, sample_width, frame_rate, num_frames, "NONE", "not compressed")
-            )
-            wav_file.writeframes(bytestream)
-        self.i += 1
-
-        # Ensure an event loop exists in the current thread
-        try:
-            asyncio.get_running_loop()
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-
-        # Play the audio file
-        Tool.run_py_pkg(f'play {tmp_file}', robot_id=self.alphamini.mini_id, debug=True)
+        self.stream.close()
+        self.device.terminate()
 
 
 class MiniSpeaker(SICConnector):
