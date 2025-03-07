0c886e88591e867aa811cd69ee97a32644330a6c
Ting-Chia Chiang <t.chiang@vu.nl>
2025-03-07 20:53:50 +0100

    Fix alphamini's dependencies

--- setup.py
+++ setup.py
@@ -60,8 +60,8 @@ extras_require = {
     ],
     "alphamini": [
         "alphamini",
-        "protobuf~=3.20.2",
-        "websockets~=13.0",
+        "protobuf~=3.20.3",
+        "websockets~=13.1",
     ],
 }
 
