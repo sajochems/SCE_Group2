bdc49f4829f8eb7b7f14baab1c8aca430f826f97
Ting-Chia Chiang <t.chiang@vu.nl>
2025-03-07 20:54:11 +0100

    Bump version to 2.0.28

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ extras_require = {
 
 setup(
     name="social-interaction-cloud",
-    version="2.0.27",
+    version="2.0.28",
     author="Koen Hindriks",
     author_email="k.v.hindriks@vu.nl",
     long_description=open("README.md").read(),
